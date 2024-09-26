12a2294a8b533eefd1d5a972ca518223ad0fdad4
asdozzz <asdozzz89@gmail.com>
2024-09-26 21:05:50 +0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,11 +7,12 @@ defineProps({
 
 const count = ref(0)
 const title = import.meta.env.VITE_APP_TITLE
+const project_title = import.meta.env.VITE_PROJECT_APP_TITLE
 const test_list = import.meta.env.VITE_APP_TEST_LIST
 </script>
 
 <template>
-  <h1>####   {{ title }} / {{test_list}}   #####</h1>
+  <h1>####   {{ title }} / {{ project_title }} / {{test_list}}   #####</h1>
 </template>
 
 <style scoped>
